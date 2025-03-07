abf54f5e137a2080a3d3f7c5dd68c9798c4e4e71
KevinSamuelTamayoGomez <l21270615@tuxtla.tecnm.mx>
2025-03-07 12:59:23 -0600

    Campos de busqueda actualizados (se muestra foto)

--- personas_proyecto/personas/views.py
+++ personas_proyecto/personas/views.py
@@ -1,21 +1,15 @@
 from django.shortcuts import render
 from rest_framework import viewsets, filters
+from rest_framework import viewsets
 from rest_framework.permissions import IsAdminUser
-
 from .models import Person
 from .serializers import PersonSerializer
-
-# Create your views here.
 
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
-    permission_classes = [IsAdminUser]
+    search_fields = ['name', 'last_name', 'role']
     filter_backends = [filters.SearchFilter]
-
-    search_fields = ['name', 'category']
-
-from django.shortcuts import render
 
 def index(request):
     return render(request, 'personas/index.html')
